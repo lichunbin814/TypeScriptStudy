cbdb620b01e68ee301c6dc27d7d9849e8b6cbd16
lcb814 <lcb814@gmail.com>
2016-01-05 23:43:09 +0800

    透過模組產生.d.ts的定義檔 - 好處在於不同模組內的class和interface可能可以相同，例如a模組裡有a1介面，則b模組也可以在裡面將介面取名為a1，且不會有相衝突的問題
    - select為多型的宣告

---
+++ WebApplication1/scripts/App/D3.d.ts
@@ -0,0 +1,39 @@
+﻿declare module D3 {
+    export interface Selectors {
+        select: {
+            (selector: string): Selection;
+            (element: EventTarget): Selection;
+        };
+    }
+
+    export interface Event {
+        x: number;
+        y: number;
+    }
+
+    export interface Base extends Selectors {
+        event: Event;
+    }
+}
+
+declare var d3: D3.Base;
+
+//以下做為可以在Module可以宣告一模一樣結構的class,interface且不會有名稱重複的問題
+declare module D3Clone {
+    export interface Selectors {
+        select: {
+            (selector: string): Selection;
+            (element: EventTarget): Selection;
+        };
+    }
+
+    export interface Event {
+        x: number;
+        y: number;
+    }
+
+    export interface Base extends Selectors {
+        event: Event;
+    }
+}
+declare var d3Clone: D3Clone.Base;
